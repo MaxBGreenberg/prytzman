30a01a2aadc578cacf84a4f361f7f24400b3f09c
MaxBGreenberg <maxbgreenberg@gmail.com>
2023-11-26 15:56:34 -0500

    First commit

---
+++ prshabboszman.sh
@@ -0,0 +1,5 @@
+# Shell script to print zmanim for shabbos of the current week to line printer
+# Uses hebcal
+#!/bin/sh
+tomorrow=$(date -d '+1 day' '+%m %d %Y')
+hebcal -tSc > ~/hebcal.tmp && hebcal -tZ | grep Sunset >> ~/hebcal.tmp && hebcal -Zc $tomorrow >> ~/hebcal.tmp && lp ~/hebcal.tmp && rm ~/hebcal.tmp
