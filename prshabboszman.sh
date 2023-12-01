567578850d63891c5d88873a8e99d5a4f193feb5
MaxBGreenberg <maxbgreenberg@gmail.com>
2023-11-30 19:18:51 -0500

    added ability to input city and set default city if none specified

--- prshabboszman.sh
+++ prshabboszman.sh
@@ -10,11 +10,14 @@
 # It will run any day of the week, but will always use today's and tomorrow's dates
 # It is therefore not useful for prining Shabbos Zmanim in advance
 #!/bin/sh
-tomorrow=$(date -d '+1 day' '+%m %d %Y')			# sets variable 'tomorrow' with the value of tomorrow's date in the fomrat mm dd yyyy
-hebcal -tSc > ~/hebcal.tmp					# Prints today's hebrew date, current week's parsha, and candle lighting times to file ~/hebcal.tmp
-hebcal -tZ | grep Plag >> ~/hebcal.tmp				# Prints today's plag hamincha time to same file
-hebcal -tZ | grep Sunset >> ~/hebcal.tmp			# Prints today's sunset time to same file
-hebcal -Zc $tomorrow >> ~/hebcal.tmp				# Prints tomorrow's zmanim and havdala time to same file
-cat ~/hebcal.tmp						# Prints file ~/hebcal.tmp to terminal
-lp ~/hebcal.tmp							# Prints file ~/hebcal.tmp to line printer
-rm ~/hebcal.tmp							# Removes file ~/hebcal.tmp
+
+DEFCITY=toronto									# sets default city for if now argument is passed
+CITY="${1:-$DEFCITY}"								# defines city we are calculating zmanim for
+tomorrow=$(date -d '+1 day' '+%m %d %Y')					# sets variable 'tomorrow' with the value of tomorrow's date in the fomrat mm dd yyyy
+hebcal -tScC $CITY > ~/hebcal.tmp						# Prints today's hebrew date, current week's parsha, and candle lighting times to file ~/hebcal.tmp
+hebcal -tZC $CITY | grep Plag >> ~/hebcal.tmp					# Prints today's plag hamincha time to same file
+hebcal -tZC $CITY | grep Sunset >> ~/hebcal.tmp					# Prints today's sunset time to same file
+hebcal -ZcC $CITY $tomorrow >> ~/hebcal.tmp					# Prints tomorrow's zmanim and havdala time to same file
+cat ~/hebcal.tmp								# Prints file ~/hebcal.tmp to terminal
+#lp ~/hebcal.tmp								# Prints file ~/hebcal.tmp to line printer
+#rm ~/hebcal.tmp								# Removes file ~/hebcal.tmp
