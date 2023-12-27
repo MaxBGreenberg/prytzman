e7ebf4d33b33286e9359131464ff0d5e82db160b
MaxBGreenberg <maxbgreenberg@gmail.com>
2023-12-26 19:16:47 -0500

    moved plag to be printed before candle lighting

--- prshabboszman.sh
+++ prshabboszman.sh
@@ -17,8 +17,8 @@ CITY="${@:-$DEFCITY}"						# Defines city we are calculating zmanim from standar
 echo "Shabbos zmanim for "$CITY > ~/hebcal.tmp			# Prints city you're printing zmanim for
 FRI=$(date -dFriday '+%m %d %Y')				# Sets variable FRI to the date of Friday this week in format taken by hebcal
 SAT=$(date -dSaturday '+%m %d %Y')				# Sets variable SAT to the date of Saturday this week in format taken by hebcal
+hebcal -ZC "${CITY}" $FRI |grep Plag >> ~hebcal.tmp		# Prints today's plag hamincha time to the same file
 hebcal -ScC "${CITY}" $FRI >> ~/hebcal.tmp			# Prints today's hebrew date, current week's parsha, and candle lighting times to file ~/hebcal.tmp
-hebcal -ZC "${CITY}" $FRI | grep Plag >> ~/hebcal.tmp		# Prints today's plag hamincha time to same file
 hebcal -ZC "${CITY}" $FRI | grep Sunset >> ~/hebcal.tmp		# Prints today's sunset time to same file
 hebcal -ZcC "${CITY}" $SAT >> ~/hebcal.tmp			# Prints tomorrow's zmanim and havdala time to same file
 cat ~/hebcal.tmp						# Prints file ~/hebcal.tmp to terminal
