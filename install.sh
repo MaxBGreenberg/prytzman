8b489ff79db714c08d9411030e904958854ad221
MaxBGreenberg <maxbgreenberg@gmail.com>
2024-01-12 15:46:00 -0500

    edited install script made group wheel instead of root gave members of wheel write permissions made install.sh executable

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Run inside prshabboszman directory
 #! /bin/sh
 mv ../prshabboszman /usr/local/lib/prshabboszman				# Move directory containing git repository to /usr/local/lib/
-chown -R root:root /usr/local/lib/prshabboszman					# Change owner to root and group to root
+chown -R root:wheel /usr/local/lib/prshabboszman				# Change owner to root and group to wheel
 chmod -R a+rx /usr/local/lib/prshabbsozman					# Add read and execute permissions for all users
+chmod -R g+w /usr/local/lib/prshabboszman					# Add write permissions for members of the wheel group
 ln -s /usr/local/lib/prshabboszman/prshabboszman.sh /usr/local/bin/psz		# Create symbolic link to prshabboszman.sh in /usr/local/bin/ called psz
