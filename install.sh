6dd123603a4fb101bb49750ca36ff2b051b019c4
MaxBGreenberg <maxbgreenberg@gmail.com>
2024-01-15 19:56:08 -0500

    fork of prshabboszman prytzman for printing yom tov zmanim first commit changed all refrerence of prshabboszman name to prytzman

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-# Shell script to install prshabboszman locally
+# Shell script to install prytzman locally
 # Run with root priveliges
-# Run inside prshabboszman directory
+# Run inside prytzman directory
 #! /bin/sh
-mv ../prshabboszman /usr/local/lib/prshabboszman				# Move directory containing git repository to /usr/local/lib/
-chown -R root:wheel /usr/local/lib/prshabboszman				# Change owner to root and group to wheel
-chmod -R a+rx /usr/local/lib/prshabbsozman					# Add read and execute permissions for all users
-chmod -R g+w /usr/local/lib/prshabboszman					# Add write permissions for members of the wheel group
-ln -s /usr/local/lib/prshabboszman/prshabboszman.sh /usr/local/bin/psz		# Create symbolic link to prshabboszman.sh in /usr/local/bin/ called psz
+mv ../prytzman /usr/local/lib/prytzman					# Move directory containing git repository to /usr/local/lib/
+chown -R root:wheel /usr/local/lib/prytzman				# Change owner to root and group to wheel
+chmod -R a+rx /usr/local/lib/prytzman					# Add read and execute permissions for all users
+chmod -R g+w /usr/local/lib/prytzman					# Add write permissions for members of the wheel group
+ln -s /usr/local/lib/prytzman/prytzman.sh /usr/local/bin/psz		# Create symbolic link to prytzman.sh in /usr/local/bin/ called psz
